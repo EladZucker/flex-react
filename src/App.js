8900139625fe15e98688e271b5a11ccf49a26e84
Elad Zucker <elad-zucker@abra-it.com>
2022-05-15 09:47:26 +0300

    .

--- src/App.js
+++ src/App.js
@@ -14,27 +14,113 @@ import laptopImage from "./Images/laptop.png";
 /* Make array with data for all columns and a field which represent which columns 
    the article should be displayed in */
 
-const leftData = [
+const data = [
   { title: "DESIGN SYSTEM",
-    headline: "How Do You Handle Component Spacing In A Design System ?",
-    buttonText: "READ THIS",
-    content: (<p>Say you’ve got a <strong>&lt; Card /&gt;</strong> component. 
-              It’s highly likely it shouldn’t be butted right up against any 
-              other components with no spacing around it. That’s true for… 
-              pretty much every component. So, how do you handle component spacing 
-              in a design system?</p>),
+  headline: "How Do You Handle Component Spacing In A Design System ?",
+  buttonText: "READ THIS",
+  content: (<p>Say you’ve got a <strong>&lt; Card /&gt;</strong> component. 
+            It’s highly likely it shouldn’t be butted right up against any 
+            other components with no spacing around it. That’s true for… 
+            pretty much every component. So, how do you handle component spacing 
+            in a design system?</p>),
+  column: 'left',
 
-    image: undefined },
+  image: undefined },
 
-    { title: "FRONTEND",
-    headline: "How to Favicon in 2022: Six files that fit most needs",
-    buttonText: undefined,
-    content: (<p>Editor’s note: With the arrival of the new year, 
-              this post has been slightly modified to reflect 
-              the fact that this content is still relevant!</p>),
-    image: laptopImage },
+  { title: "FRONTEND",
+  headline: "How to Favicon in 2022: Six files that fit most needs",
+  buttonText: undefined,
+  content: (<p>Editor’s note: With the arrival of the new year, 
+            this post has been slightly modified to reflect 
+            the fact that this content is still relevant!</p>),
+  column: 'left',
 
+  image: laptopImage },
+
+  {
+    title: 'COLORS',
+    headline: 'A whistle-stop tour of 4 new CSS color features',
+    column: 'mid',
+
+    content: (<>
+                <p>I was just writing in my <a href="#">“What’s new in since CSS3?”</a> article about 
+                   recent and possible future changes to CSS colors. It’s weirdly a lot. There are just as 
+                   many or more new and upcoming ways to define colors than what we have now. I thought we’d take a really quick look.</p>
+                <p>First, a major heads up. This stuff is so complicated. I barely understand it. But here are some aspects:</p>
+                <ul>
+                  <li>Before all this upcoming change, we only had RGB as a color model, and everything dealt with that.</li>
+                  <li>We had different “color spaces” that handled it differently (e.g. the rgb() function 
+                      mapped that RGB color model as a cube with linear coordinates, the hsl() function 
+                      mapped that RGB color model as a cylinder) but it was all sRGB gamut.</li>
+                  <li>With the upcoming changes, we’re getting new color models and (!) we’re getting 
+                      new functions that map that color model differently. So I think it’s kind of a double-triple whammy.</li>
+                </ul>
+              </>
+            ),
+            image:codeImage
+          },
+          {
+            title: 'SUBSCRIPTION',
+            headline: 'Stay tuned & get new updates',
+            column: 'mid',
+
+            content: (<p>If you'd like to know when we release additional results or announce next year's edition, just leave us your email below</p>),
+          },
+          { title: "WEB & MOBILE DEVELOPMENT",
+          headline: "Building an adaptive favicon",
+          column: 'right',
+          content: (<p>A custom favicon is a great way to polish a web project. 
+                       It displays on desktop browser tabs, and also inside 
+                       "save for later" readers, other blog posts linking to 
+                       your site, and more. Traditionally this has been done 
+                       with the .ico file type, but recently browsers have 
+                       allowed use of SVG, a vector format.</p>),
+          image: undefined },
+      
+          { title: "FRONTEND",
+          headline: "Looking for you!",
+          buttonText: undefined,
+          column: 'right',
+
+          content: (<p>As part of your role, you will have the opportunity to develop from scratch, working closely with the product, design, and QA teams to deliver the best product.
+            This is a great opportunity to join us and grow with other team members.
+            
+            Send CV to Jobsil@abra-it.com</p>),
+          image: careersImage 
+  },
+    
 ]
+let leftData = [];
+let midData = [];
+let rightData = [];
+
+const columns = [
+  { data : leftData,
+    isStandardSize : true,
+  },
+  { data : midData,
+    isStandardSize : false,
+  },
+  { data : rightData,
+    isStandardSize : true,
+  }
+];
+
+for (let i=0;i<data.length;i++) {
+  switch(data[i].column) {
+    case "left":
+      leftData.push(data[i]);
+      break;
+    case "mid":
+      midData.push(data[i]);
+      break;
+    case "right":
+      rightData.push(data[i]);
+      break;
+    default:
+  }
+}
+
 function App() {
   return (
     <div className="App">
@@ -42,7 +128,7 @@ function App() {
       <StyledTitle>Latest updates</StyledTitle>
       <ContentWrapper>
         <ColumnContainer>
-          { leftData.map(item => {
+          { leftData.map((item, idx, arr)=> {
             return (
               <>
               <Article title={item.title}
@@ -52,13 +138,13 @@ function App() {
                        >
                 {item.content}
               </Article>
-                <hr></hr>
+                {arr.length-1 !== idx && <hr></hr>}
               </>
             )
           })}
         </ColumnContainer>
         <ColumnContainer big>
-        { leftData.map(item => {
+        { midData.map((item, idx, arr) => {
             return (
               <>
               <Article title={item.title}
@@ -68,7 +154,7 @@ function App() {
                        >
                 {item.content}
               </Article>
-                <hr></hr>
+              {arr.length-1 !== idx && <hr></hr>}
               </>
             )
           })}
@@ -76,15 +162,20 @@ function App() {
         </ColumnContainer>
 
         <ColumnContainer>
-          <Article title="DESIGN SYSTEM" 
-                  headline="How Do You Handle Component Spacing In A Design System ?">
-            Say you’ve got a <strong>&lt; Card /&gt;</strong> component. It’s highly likely it shouldn’t be butted right up against any other components with no spacing around it. That’s true for… pretty much every component. So, how do you handle component spacing in a design system?
-          </Article>
-          <hr></hr>
-          <Article title="FRONTEND" 
-                  headline="How to Favicon in 2022: Six files that fit most needs">
-            Editor’s note: With the arrival of the new year, this post has been slightly modified to reflect the fact that this content is still relevant!
-          </Article>
+        { rightData.map((item, idx, arr) => {
+            return (
+              <>
+              <Article title={item.title}
+                       headline={item.headline}
+                       image={item.image}
+                       buttonText={item.buttonText}
+                       >
+                {item.content}
+              </Article>
+                {arr.length-1 !== idx && <hr></hr>}
+              </>
+            )
+          })}
         </ColumnContainer>
 
       </ContentWrapper>
